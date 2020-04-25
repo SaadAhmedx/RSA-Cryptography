7f033f1680c7b13e9e4f7ca0892fbcaf898835df
ayeshaaamir <59921536+ayeshaaamir@users.noreply.github.com>
2020-04-25 14:59:29 +0500

    ayesha.py
    done

--- RSA.py
+++ RSA.py
@@ -4,8 +4,6 @@ import maths
 p = 193
 q = 197
 
- 
-#RSA Modulus
 
 n = p * q # product of 2 numbers
 print("RSA Modulus: {}".format(n))
@@ -18,15 +16,4 @@ print("Eulers Toitent: {}".format(r))
 def egcd(e,r):
     while(r!=0):
         e,r=r,e%r
-    return e #func returning the value
-
-   Euclid's Algorithm
-def eugcd(e,r):
-    for i in range(1,r):
-        while(e!=0):
-            a,b=r//e,r%e
-            if(b!=0):
-                print("")
-#                 print("{} = {}x{} + {}".format(r,a,e,b))
-            r=e
-            e=b
+    return e #function returning the value
