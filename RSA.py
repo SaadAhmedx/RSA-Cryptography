065dbea7504bc5eaebe886e2fbf48091ea6c07ec
Ghanwa224 <61615948+Ghanwa224@users.noreply.github.com>
2020-05-19 17:55:08 +0500

    Ghanwa batool

--- RSA.py
+++ RSA.py
@@ -1,1 +1,24 @@
 
+import math
+p = 193
+q = 197
+def prime_check(a):
+    if(a==2):
+        return True
+    elif((a<2) or ((a%2)==0)):
+        return False
+    elif(a>2):
+        for i in range(2,a):
+            if not(a%i):
+                return false
+    return True
+#RSA Modulus
+n = p * q
+#Eulers Toitent
+r= (p-1)*(q-1)
+#GCD
+def egcd(e,r):
+    while(r!=0):
+        e,r=r,e%r
+    return e
+```
